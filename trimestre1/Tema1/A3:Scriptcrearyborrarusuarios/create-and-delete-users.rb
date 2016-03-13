8163a1c975294e96402997593966d37777c16323
tommyalonso <tommyrguez@gmail.com>
2016-03-13 10:45:40 +0000

    Corrección A3

--- trimestre1/Tema1/A3:Scriptcrearyborrarusuarios/create-and-delete-users.rb
+++ trimestre1/Tema1/A3:Scriptcrearyborrarusuarios/create-and-delete-users.rb
@@ -2,31 +2,28 @@
 # encoding: utf-8
 
 user=`whoami`
-puts user
-
-
-if user=="root"
-    users=`cat userslist.txt`
-    userslist=users.split("\n")
-   
-    userslist.each do |usu|
-        a=usu.split(":")
-       
-        nombre = a[0]
-        apellido = a[1]
-        email = a[2]
-        action = a[3]
-       
-        if action=="delete"
-            puts "deluser -f -r #{a[0]}"
-        end
-       
-        if action=="add"
-            puts "adduser #{a[0]}"
-        end
-    end 
+if user != "root\n"
+	puts "El usuario no es root"
+	exit
 end
 
-if user != "root"
-	puts "No tiene permisos"
+	file=`cat userslist.txt`
+	filas=file.split("\n")
+	filas.each do |linea|
+	datos=linea.split(":")
+	
+if datos[2] == ""
+	puts "El usuario #{datos[0]} no tiene email"
+else
+
+	if datos[-1] == "add"
+	system("useradd -m -s /bin/bash #{datos[0]}")
+	puts "Se ha creado el usuario #{datos[0]}"
+	
+	else if datos[-1] == "delete"
+	system("userdel -r #{datos[0]}")
+	puts "Se ha borrado el usuario #{datos[0]}"
+		end
+	end
+end
 end
